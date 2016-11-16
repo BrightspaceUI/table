22293be93bdbe50910f21f6cd66264daf58f32ca
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2016-11-16 15:05:15 -0500

    Add Shadow selectors for galen tests on browsers that support ShadowDOM

--- galen.common.config.js
+++ galen.common.config.js
@@ -15,6 +15,18 @@ this.specs = {
 		endpoint: endpoint + '?dir=rtl',
 		file: 'test/acceptance/table.rtl.gspec'
 	},
+	'd2l-table-shadow': {
+		shadow: true,
+		name: 'd2l-table-shadow',
+		endpoint: endpoint + '?dom=shadow',
+		file: 'test/acceptance/table.shadow.gspec'
+	},
+	'd2l-table-rtl-shadow': {
+		shadow: true,
+		name: 'd2l-table-rtl-shadow',
+		endpoint: endpoint + '?dir=rtl&dom=shadow',
+		file: 'test/acceptance/table.rtl.shadow.gspec'
+	},
 	'd2l-table-demo': {
 		name: 'd2l-table-demo',
 		endpoint: demoEndpoint,
