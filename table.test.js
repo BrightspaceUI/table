00201c5ae1684fc8f7c7976fbd48cfe967a4ae50
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2017-08-14 16:48:39 -0400

    Hybridize attempt

--- table.test.js
+++ table.test.js
@@ -36,23 +36,87 @@ var browsers = {
 	})
 };
 
-var endpoint = 'http://localhost:8080/components/d2l-table/demo/simple.html';
-var demoEndpoint = 'http://localhost:8080/components/d2l-table/demo/index.html';
+var mainlineEndpoint = 'http://localhost:8081/components/d2l-table/demo/simple.html';
+var xEndpoint = 'http://localhost:8000/components/d2l-table/demo/simple.html';
+var demoEndpoint = 'http://localhost:8081/components/d2l-table/demo/index.html';
 
-polymerTests(browsers, function(test) {
+polymerTests(browsers, function(test, ctx) {
+	var hasShadow = ctx.driver.executeScript('return Boolean(Element.prototype.createShadowRoot)').booleanValue();
+
+	// https://github.com/webcomponents/shadydom/issues/168
+	if (!hasShadow) {
+		test('d2l-table', {
+			endpoint: mainlineEndpoint + '?wc-shadydom&wc-shimcssproperties',
+			spec: 'test/acceptance/table.gspec',
+			tags: ['mainline', 'shady', 'shim-css', 'ltr']
+		});
+
+		test('d2l-table-rtl', {
+			endpoint: mainlineEndpoint + '?wc-shadydom&dir=rtl&wc-shimcssproperties',
+			spec: 'test/acceptance/table.rtl.gspec',
+			tags: ['mainline', 'shady', 'shim-css', 'rtl']
+		});
+
+		test('d2l-table-css', {
+			endpoint: mainlineEndpoint + '?wc-shadydom&useNativeCSSProperties=true',
+			spec: 'test/acceptance/table.gspec',
+			tags: ['mainline', 'shady', 'native-css', 'ltr']
+		});
+
+		test('d2l-table-css-rtl', {
+			endpoint: mainlineEndpoint + '?wc-shadydom&dir=rtl&useNativeCSSProperties=true',
+			spec: 'test/acceptance/table.rtl.gspec',
+			tags: ['mainline', 'shady', 'native-css', 'rtl']
+		});
+	}
+
+	test.shadow('d2l-table-shadow', {
+		endpoint: mainlineEndpoint + '?dom=shadow&useNativeCSSProperties=true',
+		spec: 'test/acceptance/table.shadow.gspec',
+		tags: ['mainline', 'shadow', 'native-css', 'ltr']
+	});
+
+	/*
+	// This spec fails because the icon mirroring is broken in Chrome's ShadowDOM
+	test.shadow('d2l-table-rtl-shadow', {
+		endpoint: endpoint + '?dir=rtl&dom=shadow',
+		spec: 'test/acceptance/table.rtl.shadow.gspec'
+	});*/
+
 	test('d2l-table', {
-		endpoint: endpoint,
-		spec: 'test/acceptance/table.gspec'
+		endpoint: xEndpoint + '?wc-shadydom',
+		spec: 'test/acceptance/table.gspec',
+		tags: ['1.x', 'shady', 'shim-css', 'ltr']
 	});
 
 	test('d2l-table-rtl', {
-		endpoint: endpoint + '?dir=rtl',
-		spec: 'test/acceptance/table.rtl.gspec'
+		endpoint: xEndpoint + '?wc-shadydom&dir=rtl',
+		spec: 'test/acceptance/table.rtl.gspec',
+		tags: ['1.x', 'shady', 'shim-css', 'rtl']
 	});
 
 	test.shadow('d2l-table-shadow', {
-		endpoint: endpoint + '?dom=shadow',
-		spec: 'test/acceptance/table.shadow.gspec'
+		endpoint: xEndpoint + '?dom=shadow',
+		spec: 'test/acceptance/table.shadow.gspec',
+		tags: ['1.x', 'shadow', 'shim-css', 'ltr']
+	});
+
+	test('d2l-table-css', {
+		endpoint: xEndpoint + '?wc-shadydom&useNativeCSSProperties=true',
+		spec: 'test/acceptance/table.gspec',
+		tags: ['1.x', 'shady', 'native-css', 'ltr']
+	});
+
+	test('d2l-table-css-rtl', {
+		endpoint: xEndpoint + '?wc-shadydom&dir=rtl&useNativeCSSProperties=true',
+		spec: 'test/acceptance/table.rtl.gspec',
+		tags: ['1.x', 'shady', 'native-css', 'rtl']
+	});
+
+	test.shadow('d2l-table-css-shadow', {
+		endpoint: xEndpoint + '?dom=shadow&useNativeCSSProperties=true',
+		spec: 'test/acceptance/table.shadow.gspec',
+		tags: ['1.x', 'shadow', 'native-css', 'ltr']
 	});
 
 	/*
