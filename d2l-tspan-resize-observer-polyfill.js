e7a731329906f4803e6d58e9755d36e9f387c227
Matt Pharoah <Matt.Pharoah@d2l.com>
2019-02-05 14:55:03 -0500

    Shuffled spaces around to appease the spiteful linting gods

--- d2l-tspan-resize-observer-polyfill.js
+++ d2l-tspan-resize-observer-polyfill.js
@@ -1,40 +1,40 @@
-if( window.ResizeObserver === undefined ) {
+if (window.ResizeObserver === undefined) {
 	// The more efficient ResizeObserver pollyfills don't work with Polymer 3
 	// due to shadow DOM complications. Fallback to this method instead.
-	
-	window.ResizeObserver = function( callback ) {
+
+	window.ResizeObserver = function(callback) {
 		this._callback = callback;
 		this._trackedNodes = [];
-		
+
 		this._checkForChanges = function() {
-			this._trackedNodes.forEach( function( trackedNode ) {
-				if(
+			this._trackedNodes.forEach(function(trackedNode) {
+				if (
 					trackedNode.ref.offsetWidth !== trackedNode.lastWidth ||
 					trackedNode.ref.offsetHeight !== trackedNode.lastHeight
 				) {
 					trackedNode.lastWidth = trackedNode.ref.offsetWidth;
 					trackedNode.lastHeight = trackedNode.ref.offsetHeight;
-					this._callback( trackedNode.ref );
+					this._callback(trackedNode.ref);
 				}
-			}.bind( this ));
-			window.requestAnimationFrame( this._checkForChanges.bind( this ) );
-		}.bind( this );
+			}.bind(this));
+			window.requestAnimationFrame(this._checkForChanges.bind(this));
+		}.bind(this);
 		this._checkForChanges();
 	};
-	
-	ResizeObserver.prototype.observe = function( node ) {
+
+	ResizeObserver.prototype.observe = function(node) {
 		this._trackedNodes.push({
 			ref: node,
 			lastWidth: node.offsetWidth,
 			lastHeight: node.offsetHeight
 		});
-		this._callback( node );
+		this._callback(node);
 	};
-	
-	ResizeObserver.prototype.unobserve = function( node ) {
-		for( var i = 0; i < this._trackedNodes.length; i++ ) {
-			if( this._trackedNodes[i].ref === node ) {
-				this._trackedNodes.splice( i, 1 );
+
+	ResizeObserver.prototype.unobserve = function(node) {
+		for (var i = 0; i < this._trackedNodes.length; i++) {
+			if (this._trackedNodes[i].ref === node) {
+				this._trackedNodes.splice(i, 1);
 				break;
 			}
 		}
