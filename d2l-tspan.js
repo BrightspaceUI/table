c50e8f30ae71d670a1730854e2348e8dff67e041
Dawood Abdullah <Dawood.Abdullah@D2L.com>
2019-02-07 11:55:59 -0500

    added custom border attribute for criterion feedback in rubrics

--- d2l-tspan.js
+++ d2l-tspan.js
@@ -1,6 +1,6 @@
 import '@polymer/polymer/polymer-legacy.js';
 import './d2l-table-observer-behavior.js';
-import './d2l-tspan-resize-observer-polyfill.js';
+import ResizeObserver from 'resize-observer-polyfill/dist/ResizeObserver.es.js';
 import './d2l-td.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
@@ -26,7 +26,13 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-tspan">
 				border-bottom: none;
 				padding: 1rem;
 				left: 0;
-				box-sizing: border-box;
+				box-sizing: border-box;			
+			}
+
+			:host([custom-border]) #float {
+				transition-property: border-color;
+				transition-timing-function: ease;
+				transition: border-color 0.5s, box-shadow 0.5s;
 			}
 
 			#float.d2l-table-row-first {
@@ -67,6 +73,21 @@ Polymer({
 			value: false,
 			reflectToAttribute: true,
 			observer: '_selectedChanged'
+		},
+		_boundFocusOutHandler: {
+			type: Function,
+			value: function() {
+				return this._focusOutHandler.bind(this);
+			}
+		},
+		customBorder: {
+			type: Boolean,
+			value: false,
+			reflectToAttribute: true,
+		},
+		feedbackInFocus: {
+			type: Boolean,
+			value: false
 		}
 	},
 	ready: function() {
@@ -75,10 +96,12 @@ Polymer({
 	attached: function() {
 		var slot = dom(this.root).querySelector('#float');
 		this._sizeObserver.observe(slot);
+		slot.addEventListener('focusout', this._boundFocusOutHandler);
 	},
 	detached: function() {
 		var slot = dom(this.root).querySelector('#float');
 		this._sizeObserver.unobserve(slot);
+		slot.removeEventListener('focusout', this._boundFocusOutHandler);
 	},
 	_selectedChanged: function(selected) {
 		this.setAttribute('aria-selected', selected.toString());
@@ -86,6 +109,36 @@ Polymer({
 	_onSizeChanged: function() {
 		if (this.root.host) {
 			this.root.host.style.height = this.$.float.offsetHeight + 'px';
+		}
+	},
+	focus: function() {
+		if (this.customBorder && !this.feedbackInFocus) {
+			this.feedbackInFocus = true;
+			this.addBorderToFloat();
+		}
+	},
+	_focusOutHandler: function(event) {
+		if (this.customBorder) {
+			this.feedbackInFocus = false;
+			this.removeBorderFromFloat();
+		}
+	},
+	addBorderToFloat: function() {
+		if (this.customBorder) {
+			var float = dom(this.root).querySelector('#float');
+			float.style.border = '2px solid var(--d2l-color-celestine)';
+			float.style.borderBottom = 'none';
+			float.style.boxShadow = '0 2px 0 var(--d2l-color-celestine)';
+			float.style.zIndex = 2;
+		}
+	},
+	removeBorderFromFloat: function() {
+		if (this.customBorder && !this.feedbackInFocus) {
+			var float = dom(this.root).querySelector('#float');
+			float.style.border = '1px solid var(--d2l-table-border-color)';
+			float.style.borderBottom = 'none';
+			float.style.boxShadow = '0 1px 0 var(--d2l-table-border-color)';
+			float.style.zIndex = 0;
 		}
 	}
 });
